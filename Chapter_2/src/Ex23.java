eb74b46444339f3bcbad83aae193d2bcd46e08bd
Jennifer Davis <JDavis190@my.gcu.edu>
2016-09-02 03:40:48 -0400

    Programming Exercise 2.23

---
+++ Chapter_2/src/Ex23.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the driving distance: ");
+		double distance = input.nextDouble();
+		
+		System.out.print("Enter miles per gallon: ");
+		double miles = input.nextDouble();
+		
+		System.out.print("Enter price per gallon: ");
+		double price  = input.nextDouble();
+		
+		double cost = (distance / miles) * price;
+		
+		double decimalFix = (int)(100 * cost);
+		double costTruncated = decimalFix / 100;
+		
+		System.out.println("The cost of driving is " + costTruncated);
+		
+	}
+
+}
