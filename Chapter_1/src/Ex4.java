03954170aba0d12bdb16052a332ed34b0877f4ab
Jennifer Davis <JDavis190@my.gcu.edu>
2016-09-02 01:50:23 -0400

    Programming Exercise 1.4

---
+++ Chapter_1/src/Ex4.java
@@ -0,0 +1,22 @@
+
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a = 94;
+		int first = 1;
+		int second = 2;
+		int third = 3;
+		int fourth = 4;
+		
+		
+		
+		System.out.println("a    a" + a + 2 + "     a" + a + 3);
+		System.out.println(first + "     " + (int)Math.pow(first,2) + "     " +(int)Math.pow(first,  3));
+		System.out.println(second + "     " + (int)Math.pow(second,2) + "     " + (int)Math.pow(second,  3));
+		System.out.println(third + "     " + (int)Math.pow(third,2) + "     " + (int)Math.pow(third,  3));
+		System.out.println(fourth + "     " + (int)Math.pow(fourth,2) + "    " + (int)Math.pow(fourth,  3));
+	}
+
+}
