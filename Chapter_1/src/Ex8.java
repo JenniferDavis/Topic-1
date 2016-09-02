a183324e20421af68992bd2488b61df82150b5ec
Jennifer Davis <JDavis190@my.gcu.edu>
2016-09-02 01:51:25 -0400

    Programming Exercise 1.8

---
+++ Chapter_1/src/Ex8.java
@@ -0,0 +1,15 @@
+
+public class Ex8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double pi = 3.14159;
+		double radius = 5.5;
+		double perimeter = 2 * radius * pi;
+		double area = 2 * radius * radius;
+		
+		System.out.println("The area of a circle with radius 5.5 is " + area + " square units.");
+		System.out.println("The perimeter of a circle with radius 5.5 is " + perimeter + " units.");
+	}
+
+}
