445815cab38cbf409281144117b1519a7133039d
Jennifer Davis <JDavis190@my.gcu.edu>
2016-09-03 18:49:57 -0400

    Updated Ex

--- Chapter_2/src/Ex20.java
+++ Chapter_2/src/Ex20.java
@@ -13,7 +13,7 @@ public class Ex20 {
 		float interest = balance * (interestRate/1200);
 		
 
-		System.out.println("The interest is " + interest);
+		System.out.println("The interest is " + Math.round( interest * 100000) / 100000.0);
 		
 	}
 
